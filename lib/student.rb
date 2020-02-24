e3045a987c61c5ada25ab0082aef34e589e59686
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-24 00:27:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,12 @@
 class Student < User  
   
-  @attr_accessor :knowledge
 
   def initialize
     @knowledge = []
   end 
 
-  def learn(string_of_knowledge)
-    @knowldge << string_of_knowledge
+  def learn(string)
+    @knowldge << string
   end 
 
   def knowledge
